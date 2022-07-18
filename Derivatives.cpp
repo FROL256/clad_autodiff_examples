775bef6ecffc7f2264c94207c7be6750e33c3a31
Vladimir Frolov <vova@frolov.pp.ru>
2022-07-18 13:19:28 +0300

    edit

--- Derivatives.cpp
+++ Derivatives.cpp
@@ -1,100 +1,72 @@
-void ProjectX_grad(float B[16], float V[4], clad::array_ref<float> _d_B, clad::array_ref<float> _d_V) {
+static inline void VS_Y_grad(float V[3], const CamInfo &data, clad::array_ref<float> _d_V, clad::array_ref<float> _d_data) {
     float _t0;
     float _t1;
     float _t2;
     float _t3;
     float _t4;
     float _t5;
+    float _d_W = 0;
     float _t6;
     float _t7;
-    float _d_W = 0;
     float _t8;
     float _t9;
+    float _t10;
+    float _t11;
+    float _t12;
+    float _t13;
+    float _d_xNDC = 0;
+    float _t14;
+    float _t15;
     _t1 = V[0];
-    _t0 = B[3];
+    _t0 = data.projM[3];
     _t3 = V[1];
-    _t2 = B[7];
+    _t2 = data.projM[7];
     _t5 = V[2];
-    _t4 = B[11];
-    _t7 = V[3];
-    _t6 = B[15];
-    float W = _t1 * _t0 + _t3 * _t2 + _t5 * _t4 + _t7 * _t6;
-    _t9 = V[0];
-    _t8 = W;
-    float ProjectX_return = _t9 / _t8;
+    _t4 = data.projM[11];
+    const float W = _t1 * _t0 + _t3 * _t2 + _t5 * _t4 + data.projM[15];
+    _t8 = V[0];
+    _t7 = data.projM[1];
+    _t10 = V[1];
+    _t9 = data.projM[5];
+    _t12 = V[2];
+    _t11 = data.projM[9];
+    _t13 = -(_t8 * _t7 + _t10 * _t9 + _t12 * _t11 + data.projM[13]);
+    _t6 = W;
+    const float xNDC = _t13 / _t6;
+    _t15 = (xNDC * 0.5F + 0.5F);
+    _t14 = data.height;
+    float VS_Y_return = _t15 * _t14;
     goto _label0;
   _label0:
     {
-        float _r8 = 1 / _t8;
-        _d_V[0] += _r8;
-        float _r9 = 1 * -_t9 / (_t8 * _t8);
-        _d_W += _r9;
+        float _r14 = 1 * _t14;
+        float _r15 = _r14 * 0.5F;
+        _d_xNDC += _r15;
+        float _r16 = _t15 * 1;
+    }
+    {
+        float _r6 = _d_xNDC / _t6;
+        float _r7 = -_r6 * _t7;
+        _d_V[0] += _r7;
+        float _r8 = _t8 * -_r6;
+        float _r9 = -_r6 * _t9;
+        _d_V[1] += _r9;
+        float _r10 = _t10 * -_r6;
+        float _r11 = -_r6 * _t11;
+        _d_V[2] += _r11;
+        float _r12 = _t12 * -_r6;
+        float _r13 = _d_xNDC * -_t13 / (_t6 * _t6);
+        _d_W += _r13;
     }
     {
         float _r0 = _d_W * _t0;
         _d_V[0] += _r0;
         float _r1 = _t1 * _d_W;
-        _d_B[3] += _r1;
         float _r2 = _d_W * _t2;
         _d_V[1] += _r2;
         float _r3 = _t3 * _d_W;
-        _d_B[7] += _r3;
         float _r4 = _d_W * _t4;
         _d_V[2] += _r4;
         float _r5 = _t5 * _d_W;
-        _d_B[11] += _r5;
-        float _r6 = _d_W * _t6;
-        _d_V[3] += _r6;
-        float _r7 = _t7 * _d_W;
-        _d_B[15] += _r7;
-    }
-}
-void NdcToScreenSpace_grad(float x, float width, clad::array_ref<float> _d_x, clad::array_ref<float> _d_width) {
-    float _t0;
-    float _t1;
-    _t1 = (x * 0.5F + 0.5F);
-    _t0 = width;
-    float NdcToScreenSpace_return = _t1 * _t0 - 0.5F;
-    goto _label0;
-  _label0:
-    {
-        float _r0 = 1 * _t0;
-        float _r1 = _r0 * 0.5F;
-        * _d_x += _r1;
-        float _r2 = _t1 * 1;
-        * _d_width += _r2;
-    }
-}
-void VS_X_grad(float V[4], float B[16], float width, clad::array_ref<float> _d_V, clad::array_ref<float> _d_B, clad::array_ref<float> _d_width) {
-    float *_t0;
-    float *_t1;
-    float _d_xNDC = 0;
-    float _t2;
-    float _t3;
-    _t0 = B;
-    _t1 = V;
-    const float xNDC = ProjectX(_t0, _t1);
-    _t2 = xNDC;
-    _t3 = width;
-    float VS_X_return = NdcToScreenSpace(_t2, _t3);
-    goto _label0;
-  _label0:
-    {
-        float _grad2 = 0.F;
-        float _grad3 = 0.F;
-        NdcToScreenSpace_grad(_t2, _t3, &_grad2, &_grad3);
-        float _r2 = 1 * _grad2;
-        _d_xNDC += _r2;
-        float _r3 = 1 * _grad3;
-        * _d_width += _r3;
-    }
-    {
-        clad::array<float> _grad0(_d_B.size());
-        clad::array<float> _grad1(_d_V.size());
-        ProjectX_grad(_t0, _t1, _grad0, _grad1);
-        clad::array_ref<float> _r0(_grad0 *= _d_xNDC);
-        _d_B += _r0;
-        clad::array_ref<float> _r1(_grad1 *= _d_xNDC);
-        _d_V += _r1;
     }
 }
--- SourceFile.cpp
+++ SourceFile.cpp
@@ -10,43 +10,131 @@
 using LiteMath::M_PI;
 using LiteMath::float3;
 
-float ProjectX(float B[16], float V[4])
+
+static inline float minD(float x, float y) { return x < y ? x : y; }
+static inline float maxD(float x, float y) { return x < y ? y : x; }
+static inline float clampD(float x, float a_min, float a_max) { return minD(a_max, maxD(x, a_min)); }
+
+
+static inline float GGX_Distribution(float cosThetaNH, float alpha)
 {
-  float W = V[0] * B[3] + V[1] * B[7] + V[2] * B[11] + V[3] * B[15]; 
-  return V[0]/W;
+  const float alpha2  = alpha * alpha;
+  const float NH_sqr  = clampD(cosThetaNH * cosThetaNH, 0.0f, 1.0f);
+  const float den     = NH_sqr * alpha2 + (1.0f - NH_sqr);
+  return alpha2 / maxD(M_PI * den * den, 1e-6f);
 }
 
-float NdcToScreenSpace(float x, float width)
+static inline float GGX_GeomShadMask(const float cosThetaN, const float alpha)
 {
-  return (x*0.5f + 0.5f)*width - 0.5f;
+  const float cosTheta_sqr = clampD(cosThetaN*cosThetaN, 0.0f, 1.0f);
+  const float tan2         = (1.0f - cosTheta_sqr) / maxD(cosTheta_sqr, 1e-6f);
+  const float GP           = 2.0f / (1.0f + std::sqrt(1.0f + alpha * alpha * tan2));
+  return GP;
 }
 
-float VS_X(float V[4], float B[16], float width)
+float EvalGGX(float l[3], float v[3], float n[3], float roughness)
 {
-  const float xNDC = ProjectX(B,V);
-  return NdcToScreenSpace(xNDC, width);
-}
+  float h[3]; // const float3 h = normalize(v + l); // half vector
+  {
+    h[0] = l[0] + v[0];
+    h[1] = l[1] + v[1];
+    h[2] = l[2] + v[2];
+    const float lenInv = 1.0f/( std::sqrt(h[0]*h[0] + h[1]*h[1] + h[2]*h[2]) ); 
+    h[0] *= lenInv;
+    h[1] *= lenInv;
+    h[2] *= lenInv;
+  }
 
-//float VS_X(float V[4], float projM[16], float width)
+  const float dotNH = n[0]*h[0] + n[1]*h[1] + n[2]*h[2]; // dot(n, h);
+  const float dotNV = n[0]*v[0] + n[1]*v[1] + n[2]*v[2]; // dot(n, v);
+  const float dotNL = n[0]*l[0] + n[1]*l[1] + n[2]*l[2]; // dot(n, l);
+  const float roughSqr = roughness * roughness;
+  const float D = GGX_Distribution(dotNH, roughSqr);
+  const float G = GGX_GeomShadMask(dotNV, roughSqr) * GGX_GeomShadMask(dotNL, roughSqr); 
+  return (D * G)/maxD(4.0f * dotNV * dotNL, 1e-6f); 
+} 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float f(float arr[2]) { return arr[0] + arr[1]; }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//static inline void mat4_colmajor_mul_vec4(float RES[4], const float B[16], const float V[4]) // modern gcc compiler succesfuly vectorize such implementation!
 //{
-//  const float W    = V[0] * projM[3] + V[1] * projM[7] + V[2] * projM[11] + V[3]*projM[15]; 
-//  const float xNDC = V[0]/W;
-//  return (xNDC*0.5f + 0.5f)*width - 0.5f;
+//	RES[0] = V[0] * B[0] + V[1] * B[4] + V[2] * B[ 8] + V[3] * B[12];
+//	RES[1] = V[0] * B[1] + V[1] * B[5] + V[2] * B[ 9] + V[3] * B[13];
+//	RES[2] = V[0] * B[2] + V[1] * B[6] + V[2] * B[10] + V[3] * B[14];
+//	RES[3] = V[0] * B[3] + V[1] * B[7] + V[2] * B[11] + V[3] * B[15];
 //}
 
-struct Uniforms
+//static inline float4 swglClipSpaceToScreenSpaceTransform(float4 a_pos, const float4 viewportf) // pre (g_pContext != nullptr)
+//{
+//  const float fw = viewportf.z;
+//  const float fh = viewportf.w;
+//
+//  const float x  = a_pos.x*0.5f + 0.5f;
+//  const float y  = a_pos.y*0.5f + 0.5f;
+//
+//  return float4(x*fw - 0.5f + viewportf.x, y*fh - 0.5f + viewportf.y, a_pos.z, a_pos.w);
+//}
+
+struct CamInfo
 {
   float projM[16];
   float width;
   float height;
 };
 
+static inline float VS_X(float V[3], const CamInfo& data)
+{
+  const float W    =  V[0] * data.projM[3] + V[1] * data.projM[7] + V[2] * data.projM[11] + data.projM[15]; 
+  const float xNDC = (V[0] * data.projM[0] + V[1] * data.projM[4] + V[2] * data.projM[ 8] + data.projM[12])/W;
+  return (xNDC*0.5f + 0.5f)*data.width;
+}
+
+static inline float VS_Y(float V[3], const CamInfo& data)
+{
+  const float W    =   V[0] * data.projM[3] + V[1] * data.projM[7] + V[2] * data.projM[11] + data.projM[15]; 
+  const float xNDC = -(V[0] * data.projM[1] + V[1] * data.projM[5] + V[2] * data.projM[ 9] + data.projM[13])/W;
+  return (xNDC*0.5f + 0.5f)*data.height;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct IgnoreMe
+{
+  float c;
+};
+
+float testFunc(float x, float y, const IgnoreMe* data)
+{
+  return x*y+data->c;
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, const char** argv)
 {
- 
-  Uniforms data = {
+  //float x = 1.0f;
+  //float y = 2.0f;
+  ////float xy[2] = {x,y};  
+  //auto f_grad1 = clad::gradient(testFunc);
+  //float result1[3] = {};
+  //
+  //IgnoreMe data;
+  //data.c = 3.0f;
+  //
+  ////f_grad1.execute(x, y, data, &result1[0],  &result1[1]);
+  ////testFunc_grad(x,y,data, &result1[0],  &result1[1]);
+  //std::cout << "dx: " << result1[0] << ' ' << "dy: " << result1[1] << std::endl;
+  
+  
+  CamInfo data = {
                     {1.0f, 0.3f, 0.2f, 0.1f,
                      0.3f, 1.0f, 0.1f, 0.1f,
                      0.2f, 0.2f, 1.0f, 0.1f,
@@ -55,20 +143,81 @@ int main(int argc, const char** argv)
 
   std::cout << "HERE(1)" << std::endl;
 
-  auto vsx_grad = clad::gradient(VS_X);
+  auto vsx_grad = clad::gradient(VS_Y);
 
   std::cout << "HERE(2)" << std::endl;
 
-  float vertex[4]       = {1,1,1,1};
-  float result1[4+16+1] = {};
+  float vertex[3] = {1,1,1};
+  float result1[3] = {};
   
-  vsx_grad.execute(vertex, data.projM, data.width,
-                   clad::array_ref<float>(&result1[0], 4), clad::array_ref<float>(&result1[4], 16), clad::array_ref<float>(&result1[20], 1));
+  //vsx_grad.execute(data.projM, vertex, data.width,
+  //                 clad::array_ref<float>(&result1[0], 16), clad::array_ref<float>(&result1[16], 4), clad::array_ref<float>(&result1[20], 1));
+
+  //VS_X_grad(data.projM, vertex, data.viewportWidth,
+  //          clad::array_ref<float>(&result1[0], 16), clad::array_ref<float>(&result1[16], 4), clad::array_ref<float>(&result1[20], 1));
 
   std::cout << "HERE(3)" << std::endl;
 
   for(int i=0;i<21;i++)
     std::cout << i << ":\t" << result1[i] << std::endl;
+  
+
+  /*
+  auto ggx_grad = clad::gradient(EvalGGX);
+
+  float3 lightDir = LiteMath::normalize(float3(-1,1,0));
+  float3 viewDir  = LiteMath::normalize(float3(+1,1,0));
+  float3 normal   = LiteMath::normalize(float3(0.25f,1,0));
+  float roughness = 0.5f;
+
+  float result1[10] = {};
+  ggx_grad.execute(lightDir.M, viewDir.M, normal.M, roughness, 
+                   &result1[0], &result1[3], &result1[6], &result1[9]);
+  
+  //EvalGGX_grad(lightDir.M, viewDir.M, normal.M, roughness, 
+  //             &result1[0], &result1[3], &result1[6], &result1[9]);
+
+  // check with brute force approach
+  // 
+  const float dEpsilon = 1e-4f;
+  const float ggxValue = EvalGGX(lightDir.M, viewDir.M, normal.M, roughness);
+  float result2[10] = {};
+
+  // 0,1,2 ==> viewDir
+  {
+    float3 lightDir1 = lightDir + float3(dEpsilon,0,0);
+    float3 lightDir2 = lightDir + float3(0,dEpsilon,0);
+    float3 lightDir3 = lightDir + float3(0,0,dEpsilon);
+    result2[0] = (EvalGGX(lightDir1.M, viewDir.M, normal.M, roughness) - ggxValue)/dEpsilon;
+    result2[1] = (EvalGGX(lightDir2.M, viewDir.M, normal.M, roughness) - ggxValue)/dEpsilon;
+    result2[2] = (EvalGGX(lightDir3.M, viewDir.M, normal.M, roughness) - ggxValue)/dEpsilon;
+  }
+
+  // 3,4,5 ==> lightDir
+  {
+    float3 viewDir1 = viewDir + float3(dEpsilon,0,0);
+    float3 viewDir2 = viewDir + float3(0,dEpsilon,0);
+    float3 viewDir3 = viewDir + float3(0,0,dEpsilon);
+    result2[3] = (EvalGGX(lightDir.M, viewDir1.M, normal.M, roughness) - ggxValue)/dEpsilon;
+    result2[4] = (EvalGGX(lightDir.M, viewDir2.M, normal.M, roughness) - ggxValue)/dEpsilon;
+    result2[5] = (EvalGGX(lightDir.M, viewDir3.M, normal.M, roughness) - ggxValue)/dEpsilon;
+  }
+
+  // 7,8,9 ==> normal
+  {
+    float3 normal1 = normal + float3(dEpsilon,0,0);
+    float3 normal2 = normal + float3(0,dEpsilon,0);
+    float3 normal3 = normal + float3(0,0,dEpsilon);
+    result2[6] = (EvalGGX(lightDir.M, viewDir.M, normal1.M, roughness) - ggxValue)/dEpsilon;
+    result2[7] = (EvalGGX(lightDir.M, viewDir.M, normal2.M, roughness) - ggxValue)/dEpsilon;
+    result2[8] = (EvalGGX(lightDir.M, viewDir.M, normal3.M, roughness) - ggxValue)/dEpsilon;
+  }
+
+  result2[9] = (EvalGGX(lightDir.M, viewDir.M, normal.M, roughness+dEpsilon) - ggxValue)/dEpsilon;
+
+  for(int i=0;i<10;i++)
+    std::cout << std::setw(10) << std::right << result1[i] << " | " << result2[i] << std::endl;
+  */
  
   return 0;
 }
